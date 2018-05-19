4f78c6948f7a2c4c00b69e859f153213fdc567ac
Brian Chotisunanta <bc.fsdev@gmail.com>
2018-05-19 07:01:57 -0700

    fixed whitespacing and changed animation style

--- animations1.js
+++ animations1.js
@@ -1,10 +1,10 @@
 $(window).scroll(function() {
 		$('.movie-content').each(function() {
-  		var imagePos = $(this).offset().top;
+				var imagePos = $(this).offset().top;
 
-  		var topOfWindow = $(window).scrollTop();
-  			if (imagePos < topOfWindow+400) {
-  				$(this).addClass("slideExpandUp");
-  			}
+				var topOfWindow = $(window).scrollTop();
+						if (imagePos < topOfWindow+400) {
+							$(this).addClass("slideExpandUp");
+						}
 		});
-	});
+});
